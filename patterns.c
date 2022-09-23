9af38308d6a948281f140593b9810d593a2f8241
AnshSheladiya <anshsheladiya83@gmail.com>
2022-09-23 09:18:34 +0530

    first commit

---
+++ patterns.c
@@ -0,0 +1,166 @@
+// #include<stdio.h>
+// int main(){
+//     int i,j;
+//     for(i=0;i<5;i++){
+//         for(j=0;j<5;j++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+// #include<stdio.h>
+// int main(){
+//     int i,j;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<=i;j++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+// #include<stdio.h>
+// int main(){
+//     int i,j;
+//     for(i=5;i>=1;i--){
+//         for(j=i;j>=1;j--){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+// * * * * *
+// * * * *
+// * * *
+// * *
+// *
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<=i;j++){
+//             printf(" ");
+//         }
+//         for(k=i;k<=5;k++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+//  *****
+//   ****
+//    ***
+//     **
+//      *
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+//         for(j=i;j<5;j++){
+//             printf(" ");
+//         }
+//         for(k=1;k<=i;k++){
+//             printf("*");
+//         }
+//         printf("\n");
+//     }
+// }
+//     *
+//    **
+//   ***
+//  ****
+// *****
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+//         for(j=5;j=i;j--){
+//             printf(" ");
+//         }
+//         for(k=1;k<=i;k++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+//     *
+//    * *
+//   * * *
+//  * * * *
+// * * * * *
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<=i;j++){
+//             printf(" ");
+//         }
+//         for(k=i;k<=5;k++){
+//             printf("* ");
+//         }
+//         printf("\n");
+//     }
+// }
+//  * * * * *
+//   * * * *
+//    * * *
+//     * *
+//      *
+
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k,Min=1;
+//     for(i=1;i<=5;i++){
+//         for(j=5;j>i;j--){
+//             printf(" ");
+//         }
+//         for(k=1;k<=Min;k++){
+//             printf("*");
+//         }
+//         Min+=2;
+//         printf("\n");
+//     }
+// }
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+
+// #include<stdio.h>
+// int main(){
+//     int i,j,k,Max=9;
+//     for(i=1;i<=5;i++){
+//         for(j=1;j<i;j++){
+//             printf(" ");
+//         }
+//         for(k=1;k<=Max;k++){
+//             printf("*");
+//         }
+//         Max-=2;
+//         printf("\n");
+//     }
+// }
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+
